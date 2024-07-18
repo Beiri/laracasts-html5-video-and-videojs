107d9b044cbaa597b909cb10f6c09e500d18bd1c
Beiri Alife <beiri.al@outlook.com>
2024-07-18 16:47:41 -0300

    Ep 02 - VideoJS Setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    {{-- <link href="https://vjs.zencdn.net/5.10.2/video-js.css" rel="stylesheet" /> --}}
+    <link href="https://vjs.zencdn.net/8.16.1/video-js.css" rel="stylesheet" />
+
     <title>Laravel</title>
 </head>
 
 <body>
     <h1>Test</h1>
-    <video controls poster="http://vjs.zencdn.net/v/oceans.png">
+    <video id="my-video" class="video-js vjs-big-play-centered" controls poster="http://vjs.zencdn.net/v/oceans.png"
+        data-setup="{}">
         <source src="http://vjs.zencdn.net/v/oceans.mp4" type="video/mp4">
 
-        <p>Please upgrade your browser.</p>
+        <p class="vjs-no-js">
+            To view this video please enable JavaScript, and consider upgrading to a
+            web browser that
+            <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+        </p>
     </video>
+
+    {{-- <script src="https://vjs.zencdn.net/5.10.2/video.min.js"></script> --}}
+    <script src="https://vjs.zencdn.net/8.16.1/video.min.js"></script>
 </body>
 
 </html>
